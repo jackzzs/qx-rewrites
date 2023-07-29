64789c77d88dcd21f749b9e7dc6eedf1063e2ba2
Jack Zhou <jackzzs@outlook.com>
2023-07-30 07:26:59 +0800

    Update suishenglu.js

--- suishenglu.js
+++ suishenglu.js
@@ -13,21 +13,21 @@ hostname = api.suishenglu.cn
 
 let obj = JSON.parse($response.body);
 
-if (obj['Data'] && obj['Data']['accountModel'] && obj['Data']['accountModel']['VIP']) {
+if (obj['Data'].hasOwnProperty('accountModel')) {
   obj['Data']['accountModel']['VIP'] = 1;
   obj['Data']['accountModel']['Days'] = 1825;
   obj['Data']['accountModel']['NoAds'] = 1;
   obj['Data']['accountModel']['CurrencyAmount'] = 9999;
 }
 
-if (obj['Data'] && obj['Data']['VIP']) {
+if (obj['Data'].hasOwnProperty('VIP')) {
   obj['Data']['VIP'] = 1;
   obj['Data']['Days'] = 1825;
   obj['Data']['NoAds'] = 1;
   obj['Data']['CurrencyAmount'] = 9999;
 }
 
-if (obj['Data'] && obj['Data']['Balance']) {
+if (obj['Data'].hasOwnProperty('Balance')) {
   obj['Data']['Balance'] = 9999;
   obj['Data']['NlsPrice'] = 1;
   obj['Data']['FileToTextPrice'] = 1;
