ac1545a12b9232a0e61336887bde95b8dfa2d1df
Jack Zhou <jackzzs@outlook.com>
2023-07-30 07:21:00 +0800

    Update suishenglu.js

--- suishenglu.js
+++ suishenglu.js
@@ -3,7 +3,7 @@
 ***************************************
 [rewrite_local]
 
-^https?:\/\/api\.suishenglu\.cn\/account\/(getaccount|GetFileList) url script-response-body https://raw.githubusercontent.com/jackzzs/qx-rewrites/main/suishenglu.js
+^https?:\/\/api\.suishenglu\.cn\/account\/(getaccount|GetFileList|getUserBalance) url script-response-body https://raw.githubusercontent.com/jackzzs/qx-rewrites/main/suishenglu.js
 
 [mitm]
 
@@ -12,8 +12,25 @@ hostname = api.suishenglu.cn
 ***************************************/
 
 let obj = JSON.parse($response.body);
-obj['Data']['VIP'] = 1;
-obj['Data']['Days'] = 1825;
-obj['Data']['NoAds'] = 1;
-obj['Data']['CurrencyAmount'] = 9999;
+
+if (obj['Data'] && obj['Data']['accountModel'] && obj['Data']['accountModel']['VIP']) {
+  obj['Data']['accountModel']['VIP'] = 1;
+  obj['Data']['accountModel']['Days'] = 1825;
+  obj['Data']['accountModel']['NoAds'] = 1;
+  obj['Data']['accountModel']['CurrencyAmount'] = 9999;
+}
+
+if (obj['Data'] && obj['Data']['VIP']) {
+  obj['Data']['VIP'] = 1;
+  obj['Data']['Days'] = 1825;
+  obj['Data']['NoAds'] = 1;
+  obj['Data']['CurrencyAmount'] = 9999;
+}
+
+if (obj['Data'] && obj['Data']['Balance']) {
+  obj['Data']['Balance'] = 9999;
+  obj['Data']['NlsPrice'] = 1;
+  obj['Data']['FileToTextPrice'] = 1;
+}
+
 $done({body: JSON.stringify(obj)});
